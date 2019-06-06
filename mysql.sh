9cc287a0d9d8fbbbb5e2af2a095a78c9183926ba
Tamilvananb <43129537+Tamilvananb@users.noreply.github.com>
2019-06-06 15:15:16 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/tomcat8/
-mysql -h azuretam.mysql.database.azure.com -u zippyops@azuretam -pZippy0ps@123 -D azuredb < table.sql
+mysql -h azuretam.mysql.database.azure.com -u zippyops@azuretam -pZippy0ps@123 -D azuredb -e "create table registration (name varchar(30),email varchar(30) primary key,gender varchar(7),password varchar(30))"
