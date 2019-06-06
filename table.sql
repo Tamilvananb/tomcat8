456e63702156bec73f967274b02b02a0dfe9ca24
Tamilvananb <43129537+Tamilvananb@users.noreply.github.com>
2019-06-06 11:58:33 +0530

    Update table.sql

--- table.sql
+++ table.sql
@@ -4,5 +4,4 @@ CREATE TABLE IF NOT EXISTS `registration` (
         `gender` varchar(7) NOT NULL,
         `password` varchar(30) NOT NULL,
     PRIMARY KEY (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-INSERT INTO `registration` ( `name`, `email`, `gender`, `password` );
+) ENGINE=InnoDB;
