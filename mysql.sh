80af2d92d46dd77bed753f5729ccdd362ee2cb03
Tamilvananb <43129537+Tamilvananb@users.noreply.github.com>
2019-06-06 15:54:36 +0530

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sleep 30s
 mysql -h azuretam.mysql.database.azure.com -u zippyops@azuretam -pZippy0ps@123 -D azuredb -e "create table registration (name varchar(30),email varchar(30) primary key,gender varchar(7),password varchar(30))"
-systemctl restart mysql
+
